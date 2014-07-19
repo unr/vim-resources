0a70f9690ea0d5de900181f3b2a4f467536a3540
Paul Morrison <paul.unr@gmail.com>
2014-07-19 14:03:25 -0400

    Cleaned up unecessarry roots markup.

--- web/app/themes/vim-roots/page.php
+++ web/app/themes/vim-roots/page.php
@@ -1,4 +1,10 @@
+<?php
+/**
+ * Single Page Template
+ */
+?>
+
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+	<h1><?php the_title(); ?></h1>
+	<?php the_content(); ?>
 <?php endwhile; ?>
--- web/app/themes/vim-roots/single.php
+++ web/app/themes/vim-roots/single.php
@@ -1,1 +1,13 @@
-<?php get_template_part('templates/content', 'single'); ?>
+<?php while (have_posts()) : the_post(); ?>
+  <article <?php post_class(); ?>>
+    <header>
+      <h1 class="entry-title"><?php the_title(); ?></h1>
+    </header>
+    <div class="entry-content">
+      <?php the_content(); ?>
+    </div>
+    <footer>
+      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+    </footer>
+  </article>
+<?php endwhile; ?>
